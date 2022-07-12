d0379838e12504d3ac271775d42d55ea8d27d3b1
Dmitry Svyatov <dimka201993@gmail.com>
2022-07-12 13:09:18 +0300

    ДЗ

---
+++ 36/Program.cs
@@ -0,0 +1,52 @@
+﻿int size= InputInt("Введите размер ");
+
+int[]numbers=new int[size];
+
+int summ=0;
+
+FillArray(numbers);
+PrintArray(numbers);
+
+for(int i=0;i < numbers.Length; i++)
+{
+    if(i%2!=0)
+    {
+        summ= summ + numbers[i];
+    }
+}
+
+Console.WriteLine($"->{summ}");
+
+
+
+
+
+
+
+
+
+
+
+int InputInt(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray (int[] array)
+{
+    for(int i=0;i< array.Length; i++)
+    {
+        array[i]= new Random().Next(-100,100);
+    }
+}
+
+
+void PrintArray (int[] array)
+{
+    for(int i=0;i< array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+}
---
+++ 38/Program.cs
@@ -0,0 +1,56 @@
+﻿int size= InputInt("Введите размер массива  ");
+
+double[]numbers=new double[size];
+
+double diff=0;
+
+FillArray(numbers);
+PrintArray(numbers);
+
+double maxIndex=numbers[0];
+double minIndex=numbers[0];
+
+
+for(int i=1;i < numbers.Length; i++)
+{
+
+    if(numbers[i]>maxIndex)
+    {
+        maxIndex=numbers[i];
+    }
+    else if (numbers[i]<minIndex)
+    {
+        minIndex=numbers[i];
+    }
+}
+Console.WriteLine("Масксимальное число ="+ maxIndex);
+Console.WriteLine("Минимальное число =" + minIndex);
+diff= maxIndex-minIndex;
+Console.WriteLine("Разница максимального числа и минимального числа ="+ diff);
+
+
+
+
+int InputInt(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray (double[] array)
+{
+    for(int i=0;i< array.Length; i++)
+    {
+        array[i]= new Random().Next(0,10) + new Random().NextDouble();
+    }
+}
+
+
+void PrintArray (double[] array)
+{
+    for(int i=0;i< array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+}
